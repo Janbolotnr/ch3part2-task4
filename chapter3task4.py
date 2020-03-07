84bc324b3e9b6ad7939d48b1ef3a45cd1a9a22d8
Janbolotnr <janbolotnr@gmail.com>
2020-03-08 00:01:08 +0600

    chap3part2task4

---
+++ chapter3task4.py
@@ -0,0 +1,15 @@
+#zadacha4 cahpter3
+def mfunc():
+    text = input("nums: ").split(" ")
+    for i in range(len(text)):
+        text[i] = int(text[i])
+    if max(text)<=0:
+        print(1)
+    else:
+        for i in range(1, max(text)+ 3):
+            if i > 0 and i not in text:
+                print(i)
+                break
+            else:
+                continue
+mfunc()
